fd2458a8b80d9a115a6682fa37f9ddbd16975484
shamimsaj <shamimeforb@gmail.com>
2020-11-08 10:27:38 +0600

    commit style changes

--- elementor-support/search/view.php
+++ elementor-support/search/view.php
@@ -15,16 +15,17 @@ $alt  = $imgs['id'] ? $imgs['id'] : '';
 <div class="adv-filter">
 	<?php if ( $img ) { ?>
 		<div class="adv-filter__bg">
-			<img src="<?php echo Helper::get_img( 'shape.png' ); ?>" class="img-fluid" alt="<?php echo esc_attr( $alt ); ?>">
+			<img src="<?php echo Helper::get_img( 'home-bg.png' ); ?>" class="img-fluid" alt="<?php echo esc_attr( $alt ); ?>">
 		</div>
 	<?php } ?>
 	<div class="container">
 		<div class="row flex-lg-nowrap align-items-center">
-			<div class="adv-xl-6 col-lg-6 order-lg-1 order-2">
+			<div class="col-lg-10 offset-lg-1">
 				<?php if ( $data['title1'] || $data['title2'] ) { ?>
 					<div class="adv-filter__header">
 						<div class="section-title">
-							<p><?php echo esc_attr( $data['title1'] ); ?></br><strong><?php echo esc_attr( $data['title2'] ); ?></strong></p>
+							<h1><?php echo esc_attr( $data['title1'] ); ?></h1>
+							<p><?php echo esc_attr( $data['title2'] ); ?></p>
 						</div>
 					</div>
 				<?php } ?>
@@ -33,101 +34,55 @@ $alt  = $imgs['id'] ? $imgs['id'] : '';
 						<div class="row">
 							<div class="col-lg-12">
 								<div class="adv-filter-content__tab">
-									<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+									<ul class="nav nav-pills" id="pills-tab" role="tablist">
 										<li class="nav-item" role="presentation">
 											<a class="nav-link active" id="pills-home-tab" data-toggle="pill"
 												href="#pills-home" role="tab" aria-controls="pills-home"
-												aria-selected="true">Buy</a>
+												aria-selected="true"><i class="la la-bullhorn"></i> Events</a>
 										</li>
 										<li class="nav-item" role="presentation">
 											<a class="nav-link" id="pills-profile-tab" data-toggle="pill"
 												href="#pills-profile" role="tab" aria-controls="pills-profile"
-												aria-selected="false">Rent</a>
+												aria-selected="false"><i class="la la-home"></i> Venue</a>
 										</li>
 									</ul>
 									<div class="tab-content" id="pills-tabContent">
 										<div class="tab-pane fade show active" id="pills-home" role="tabpanel"
 											aria-labelledby="pills-home-tab">
-											<div class="row">
-												<div class="col-lg-6">
-													<div class="search-wrapper adv-looking-field">
-														<!-- Start Event Area -->
-														<select class="js-example-basic-single js-states form-control"
-															id="adv-looking-field">
-															<option value="all">Any Make</option>
-															<option value="1">Acura</option>
-															<option value="2">Alfa Romeo</option>
-															<option value="3">Audi</option>
-															<option value="4">BMW</option>
-															<option value="5">Buick</option>
-															<option value="6">Cadillac</option>
-														</select>
-														<!-- End Event Area -->
-													</div><!-- ends: .search-wrapper -->
+											<div class="adv-search-form">
+												<div class="form-group adv-input-text">
+													<input type="text" placeholder="Search events or categories">
 												</div>
-												<div class="col-lg-6">
-													<div class="search-wrapper adv-search-field">
-														<!-- Start Event Area -->
-														<select class="js-example-basic-single js-states form-control"
-															id="adv-search-field">
-															<option value="all">Any Modal</option>
-															<option value="1">All Modals</option>
-															<option value="2">Integra</option>
-															<option value="3">Legend</option>
-															<option value="4">MDX</option>
-															<option value="5">NSX</option>
-															<option value="6">NSXT</option>
-														</select>
-														<!-- End Event Area -->
-													</div><!-- ends: .search-wrapper -->
+												<div class="form-group adv-input-location">
+													<input type="text" placeholder="Any location">
 												</div>
-												<div class="col-lg-6">
-													<div class="search-wrapper adv-address-field">
-														<div class="nav_right_module search_module">
-															<form action="/">
-																<input type="text"
-																	class="form-control search_field top-search-field"
-																	placeholder="Address, city, zip..."
-																	autocomplete="off">
-															</form>
-														</div>
-														<div class="search-categories">
-															<ul class="list-unstyled">
-																<li><a href="">Recently Viewed</a></li>
-																<li><a href="">New York, NY</a></li>
-																<li><a href="">Los Angeles, CA</a></li>
-																<li><a href="">San Diego, CA</a></li>
-															</ul>
-														</div>
-													</div><!-- ends: .search-wrapper -->
+												<div class="form-group adv-input-date">
+													<input type="text" id="adv-input-date" placeholder="Any Date">
 												</div>
-												<div class="col-lg-6">
-													<div class="price-range-slider">
-														<p class="range-value">
-															<span>Price ranges:</span>
-															<input type="text" id="amount" readonly>
-														</p>
-														<div id="slider-range" class="range-bar"></div>
-													</div>
+												<div class="form-group adv-search-btn">
+													<button class="btn btn-primary">Search</button>
 												</div>
 											</div>
 										</div>
 										<div class="tab-pane fade" id="pills-profile" role="tabpanel"
 											aria-labelledby="pills-profile-tab">
-											<div class="row">
-												<div class="col-lg-6"></div>
-												<div class="col-lg-6"></div>
-												<div class="col-lg-6"></div>
-												<div class="col-lg-6"></div>
+											<div class="adv-search-form">
+												<div class="form-group adv-input-text">
+													<input type="text" placeholder="Search events or categories">
+												</div>
+												<div class="form-group adv-input-location">
+													<input type="text" placeholder="Any location">
+												</div>
+												<div class="form-group adv-input-date">
+													<input type="text" id="adv-input-date" placeholder="Any Date">
+												</div>
+												<div class="form-group adv-search-btn">
+													<button class="btn btn-primary">Search</button>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-								<?php if ( $data['search'] ) { ?>
-									<a href="#" class="btn btn-primary btn-md content-center mt-10 adv-filter__btn">
-										<i class="las la-search"></i><?php echo esc_attr( $data['search'] ); ?>
-									</a>
-								<?php } ?>
 							</div>
 						</div>
 					</div>
